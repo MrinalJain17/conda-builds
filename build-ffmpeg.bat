27000f3be3a70c0d43bc3ee0e1da1d6e5955faaf
Mrinal Jain <mrinaljain007@gmail.com>
2018-12-13 21:02:24 +0530

    Removing windows deployment

--- build-ffmpeg.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-SET VERSION=4.1
-
-call conda-build ffmpeg --numpy 1.14 --output-folder .\dump\ffmpeg
-call anaconda -v -t $ANACONDA_TOKEN upload "./dump/ffmpeg/win-64/ffmpeg-%version%-0.tar.bz2"
-call conda build purge
--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -2,7 +2,5 @@ version="4.1"
 
 conda-build ffmpeg --numpy 1.14 --output-folder ./dump/ffmpeg && cd ./dump/ffmpeg && 
 conda convert --platform osx-64 "./linux-64/ffmpeg-$version-0.tar.bz2" -o . && 
-conda convert --platform win-64 "./osx-64/ffmpeg-$version-0.tar.bz2" -o . && 
 anaconda -v -t $ANACONDA_TOKEN upload "./linux-64/ffmpeg-$version-0.tar.bz2" && 
-anaconda -v -t $ANACONDA_TOKEN upload "./win-64/ffmpeg-$version-0.tar.bz2" && 
 anaconda -v -t $ANACONDA_TOKEN upload "./osx-64/ffmpeg-$version-0.tar.bz2" && conda-build purge
