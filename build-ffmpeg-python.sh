5913fb1c2bf2b9b60cb009c73a1efed322184f10
Mrinal Jain <mrinaljain007@gmail.com>
2018-12-14 11:40:33 +0530

    Initial conda recipe for `ffmpeg-python` and automated deployment

---
+++ build-ffmpeg-python.sh
@@ -0,0 +1,8 @@
+version="0.1.16"
+
+conda-build ffmpeg-python --numpy 1.14 --output-folder ./dump/ffmpeg_python && cd ./dump/ffmpeg_python && 
+conda convert --platform win-64 "./linux-64/ffmpeg-python-$version-py36_0.tar.bz2" -o . && 
+conda convert --platform osx-64 "./linux-64/ffmpeg-python-$version-py36_0.tar.bz2" -o . && 
+anaconda -v -t $ANACONDA_TOKEN upload "./linux-64/ffmpeg-python-$version-py36_0.tar.bz2" && 
+anaconda -v -t $ANACONDA_TOKEN upload "./win-64/ffmpeg-python-$version-py36_0.tar.bz2" && 
+anaconda -v -t $ANACONDA_TOKEN upload "./osx-64/ffmpeg-python-$version-py36_0.tar.bz2" && conda-build purge
