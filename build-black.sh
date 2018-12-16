8b1ef5a2506f55f40f3cfd84543b957b53d25cf9
Mrinal Jain <mrinaljain007@gmail.com>
2018-12-16 18:21:23 +0530

    Updating version of `black`

--- build-black.sh
+++ build-black.sh
@@ -1,4 +1,4 @@
-version="18.6b4"
+version="18.9b0"
 
 conda-build black --numpy 1.14 --output-folder ./dump/black && cd ./dump/black && 
 conda convert --platform win-64 "./linux-64/black-$version-py36_0.tar.bz2" -o . && 
