75d7e7c029fa7fb7378918f0fccf006f6c6bc057
Mrinal Jain <mrinaljain007@gmail.com>
2018-12-21 12:48:44 +0530

    Update build-skorch.sh

--- build-skorch.sh
+++ build-skorch.sh
@@ -1,8 +1,8 @@
 version="0.3.0"
 
 conda-build skorch --numpy 1.14 --output-folder ./dump/skorch && cd ./dump/skorch && 
-conda convert --platform win-64 "./linux-64/*.tar.bz2" -o . && 
-conda convert --platform osx-64 "./linux-64/*.tar.bz2" -o . && 
-anaconda -v -t $ANACONDA_TOKEN upload "./linux-64/*.tar.bz2" && 
-anaconda -v -t $ANACONDA_TOKEN upload "./win-64/*.tar.bz2" && 
-anaconda -v -t $ANACONDA_TOKEN upload "./osx-64/*.tar.bz2" && conda-build purge
+conda convert --platform win-64 ./linux-64/*.tar.bz2 -o . && 
+conda convert --platform osx-64 ./linux-64/*.tar.bz2 -o . && 
+anaconda -v -t $ANACONDA_TOKEN upload ./linux-64/*.tar.bz2 && 
+anaconda -v -t $ANACONDA_TOKEN upload ./win-64/*.tar.bz2 && 
+anaconda -v -t $ANACONDA_TOKEN upload ./osx-64/*.tar.bz2 && conda-build purge
